e31a2e5112e435d5c2f9f0b3c8fe41ee0f2afc7c
Duvan Roque <dsmith.mr@gmail.com>
2024-03-11 21:44:33 -0500

    Added Cloudinary

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { request, response } = require("express");
-const { subirArchivo } = require("../helpers");
+const { subirArchivo, subirArchivoCloudinary } = require("../helpers");
 const { Usuario, Producto } = require('../models');
 
 const cargarArchivos = async (req = request, res = response) => {
@@ -92,8 +96,98 @@ const mostraImagen = async (req = request, res = response) => {
     res.sendFile(pathPlaceHolder);
 }
 
+const cargarArchivosCloudinary = async (req = request, res = response) => {
+    try {
+        // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
+        const nombre = await subirArchivoCloudinary(req.files, undefined, 'imgs');
+        // const nombre = await subirArchivo(req.files);
+        res.json({ nombre });
+    } catch (msg) {
+        res.status(400).json({ msg })
+    }
+};
+
+const actualizarImagenCloudinary = async (req = request, res = response) => {
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+            break;
+        default:
+            return res.status(500).json({ msg: `Falta validar ${coleccion}` });
+    }
+
+    if (modelo.img) {
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length - 1];
+        const [public_id] = nombre.split('.');
+        cloudinary.uploader.destroy(public_id);
+    }
+
+    const { tempFilePath } = req.files.file;
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+    modelo.img = secure_url;
+
+    await modelo.save();
+
+    res.json(modelo);
+}
+
+const mostraImagenCloudinary = async (req = request, res = response) => {
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+            break;
+        default:
+            return res.status(500).json({ msg: `Falta validar ${coleccion}` });
+    }
+
+    if (modelo.img) {
+        return res.send(modelo.img);
+    }
+
+    const pathPlaceHolder = path.join(__dirname, '../assets/no-image.jpg');
+    res.sendFile(pathPlaceHolder);
+}
+
 module.exports = {
     cargarArchivos,
     actualizarImagen,
-    mostraImagen
+    mostraImagen,
+    cargarArchivosCloudinary,
+    actualizarImagenCloudinary,
+    mostraImagenCloudinary,
 }
--- helpers/subir-archivo.js
+++ helpers/subir-archivo.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const subirArchivo = (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'], carpeta = '') => {
     return new Promise((resolve, reject) => {
         const { file } = files;
@@ -27,6 +30,26 @@ const subirArchivo = (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'],
     });
 }
 
+const subirArchivoCloudinary = async (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'], carpeta = '') => {
+    return new Promise(async (resolve, reject) => {
+        const { file } = files;
+
+        const nombreCortado = file.name.split('.');
+        const extension = nombreCortado[nombreCortado.length - 1];
+
+        if (!extensionesValidas.includes(extension)) {
+            return reject(
+                `La extension ${extension} no es permitida, ${extensionesValidas}.`
+            );
+        }
+
+        const { tempFilePath } = file;
+        const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+        resolve(secure_url);
+    });
+}
+
 module.exports = {
-    subirArchivo
+    subirArchivo,
+    subirArchivoCloudinary
 };
